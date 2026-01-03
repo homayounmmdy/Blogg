83808bf0ee21266ef718abe0301682e9ec73bded
Homayoun <homayoun763@gmail.com>
2026-01-03 15:53:18 +0330

    Using Loaders to Fetch Data

--- anthology/src/components/task-list/task-list.stories.tsx
+++ anthology/src/components/task-list/task-list.stories.tsx
@@ -6,24 +6,28 @@ import { TaskListProvider } from './task-list-context';
 const meta = {
   title: 'Components/TaskList',
   component: TaskList,
+  loaders: [
+    async () => {
+      const tasks = await fetch('https://jsonplaceholder.typicode.com/todos').then((response) =>
+        response.json(),
+      );
+      return { tasks };
+    },
+  ],
   decorators: [
-    (Story , context) => {
+    (Story, context) => {
       return (
-        <TaskListProvider tasks={[
-          {id : '1', title : 'Task 1' , completed : false},
-          {id : '2', title : 'Task 2' , completed : true},
-          {id : '3', title : 'Task 3' , completed : false},
-        ]}>
-          <Story {...context}/>
+        <TaskListProvider
+          tasks={context.loaded.tasks}
+        >
+          <Story {...context} />
         </TaskListProvider>
-      )
-    }
+      );
+    },
   ],
 } as Meta<typeof TaskList>;
 
 export default meta;
 type Story = StoryObj<typeof TaskList>;
 
-export const Default : Story = {
-
-}
+export const Default: Story = {};
